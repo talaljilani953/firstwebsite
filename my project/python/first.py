a4066c76464c877708afc50a984779ebea848b2a
Syed Talal Jilani <talaljilani79@gmail.com>
2022-02-24 16:57:20 +0500

    new website

---
+++ my project/python/first.py
@@ -0,0 +1,8 @@
+
+#[] list
+#() tuple
+#{} dictionary
+# def fun(): method
+#if a:b : is equal in 
+# [0:5:2] start and end index of list and string slicing third show number of index cover on jump
+# """ is multi line command 
